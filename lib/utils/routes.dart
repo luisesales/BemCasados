af4103e3b3ce405829b5feb36d9323f1df8b5f65
luisesales <duduosales@hotmail.com>
2024-12-02 23:05:01 -0300

    adding first view of Login and selectUser screens

---
+++ lib/utils/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const SELECT_USER = '/select-user';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const FORGOT_PASSWORD = '/forgot-password';
+}
